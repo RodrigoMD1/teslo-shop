17a0efed765eec6a9dce1dcbb06fe9be9b9d0f9c
rodrigo <rodrigo.martinez224@gmail.com>
2024-05-02 02:11:48 -0300

    V10

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,50 @@
+import { Column, Entity, PrimaryGeneratedColumn, } from "typeorm";
+
+
+
+
+@Entity('users')
+export class User {
+
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    /////////////////////////////////////////////////////
+
+    @Column('text', {
+        unique: true,
+    })
+    email: string;
+
+    /////////////////////////////////////////////////////
+
+    @Column('text')
+    password: string;
+
+    /////////////////////////////////////////////////////
+
+    @Column('text', {
+        unique: true,
+    })
+    fullName: string;
+
+    /////////////////////////////////////////////////////
+
+    @Column('bool', {
+        unique: true
+    })
+    isActive: boolean;
+
+    /////////////////////////////////////////////////////
+
+    @Column('text', {
+        array: true,
+        default: ['user']
+    })
+    roles: string[];
+
+    /////////////////////////////////////////////////////
+
+
+}
