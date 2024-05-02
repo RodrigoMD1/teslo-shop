b302134e0d1675c685dc0d07627998f5777e245e
rodrigo <rodrigo.martinez224@gmail.com>
2024-05-02 01:58:21 -0300

    v9

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { AuthModule } from './auth/auth.module';
 
 
 
@@ -36,6 +37,8 @@ import { ServeStaticModule } from '@nestjs/serve-static';
     SeedModule,
 
     FilesModule,
+
+    AuthModule,
 
   ],
 
