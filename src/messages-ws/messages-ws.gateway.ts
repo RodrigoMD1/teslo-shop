ae769c13b30699e688b162f8ccdef29540270c8a
rodrigo <rodrigo.martinez224@gmail.com>
2024-05-08 01:30:34 -0300

    v7

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,26 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class MessagesWsGateway {
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+
+
+@WebSocketGateway({ cors: true, namespace: '/' })
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+
+  constructor(
+    private readonly messagesWsService: MessagesWsService
+
+
+  ) { }
+  handleConnection(client: Socket) {
+    console.log('cliente conectado', client.id);
+
+  }
+  handleDisconnect(client: Socket) {
+    console.log('cliente desconectado', client.id);
+  }
+
+
+
 }
