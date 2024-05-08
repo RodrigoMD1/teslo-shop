996c145177e10e95b6ba29f2f027c4dc0f84563d
rodrigo <rodrigo.martinez224@gmail.com>
2024-05-08 05:12:11 -0300

    v9

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,26 +1,90 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dto/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from '../auth/interfaces';
 
 
 
 @WebSocketGateway({ cors: true, namespace: '/' })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
+  @WebSocketServer() wss: Server;
 
   constructor(
-    private readonly messagesWsService: MessagesWsService
-
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService
 
   ) { }
-  handleConnection(client: Socket) {
-    console.log('cliente conectado', client.id);
+
+  ////////////////////////////////////////////////////////////////////////////////
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.authentication as string;
+    let payload: JwtPayload;
+    try {
+
+      payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.id);
+
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+
+    //console.log({payload});
+
+
+
+    // console.log('cliente conectado', client.id);
+
+
+    // console.log({ conectados: this.messagesWsService.getConnectedClients() });   // esto permite mostrar cuantos hay conectados 
+
+    //* para avisar a todos que estas conectado en el servidor ,igual hay que poner que el cliente escuche este evento
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients())
+
+
 
   }
+
+  ////////////////////////////////////////////////////////////////////////////////
   handleDisconnect(client: Socket) {
-    console.log('cliente desconectado', client.id);
-  }
+    // console.log('cliente desconectado', client.id);
 
+    this.messagesWsService.removeClient(client.id);
+
+
+    // console.log({ conectados: this.messagesWsService.getConnectedClients() });
+
+    // *esto para mostar cuando alguien de desconecta 
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients())
+  }
+  ////////////////////////////////////////////////////////////////////////////////
+
+  // message-from-client
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient(client: Socket, payload: NewMessageDto) {
+
+    // !emite unicamente al cliente 
+    //client.emit('message-from-server', {
+    // fullName: 'soy yo ',
+    // message: payload.message || 'no-message!!' });
+
+    //!emitir a todos menos al cliente inicial seria la persona que esta escribiendo el mensaje originalmente 
+    //client.broadcast.emit('message-from-server', {
+    //  fullName: 'soy yo ',
+    // message: payload.message || 'no-message!!'});
+
+
+    // !esto se lo emite a todos incluso a la persona que lo escribio el mensaje originalmente
+    this.wss.emit('message-from-server', {
+      fullName: this.messagesWsService.getUserFullName(client.id),
+      message: payload.message || 'no-message!!'
+    });
+
+  }
+  ////////////////////////////////////////////////////////////////////////////////
 
 
 }
--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -1,4 +1,75 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Socket } from 'socket.io';
+import { User } from 'src/auth/entities/user.entity';
+import { Repository } from 'typeorm';
+
+
+interface ConnectedClients {
+    [id: string]: {
+        socket: Socket,
+        user: User,
+    }
+
+}
+
 
 @Injectable()
-export class MessagesWsService {}
+export class MessagesWsService {
+
+    private connectedClients: ConnectedClients = {}
+
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>
+    ) { }
+
+
+
+    async registerClient( client: Socket, userId: string ) {
+
+        const user = await this.userRepository.findOneBy({ id: userId });
+        if ( !user ) throw new Error('User not found');
+        if ( !user.isActive ) throw new Error('User not active');
+
+        this.checkUserConnection( user );
+
+        this.connectedClients[client.id] = {
+            socket: client,
+            user: user,
+        };
+    }
+    // estos dos es para tener registrados quien se conecto 
+
+    removeClient(clientId: string) {
+        delete this.connectedClients[clientId]
+    }
+
+
+    // conteo de los clientes 
+    getConnectedClients(): string[] {
+
+
+        return Object.keys(this.connectedClients);
+    }
+
+
+    getUserFullName(socketId: string) {
+        return this.connectedClients[socketId].user.fullName;
+    }
+
+
+    // esto para que no se repita el usuario conectado 
+    private checkUserConnection(user: User) {
+        for (const clientId of Object.keys(this.connectedClients)) {
+
+            const connectedClient = this.connectedClients[clientId];
+
+            if (connectedClient.user.id === user.id) {
+                connectedClient.socket.disconnect();
+                break;
+            }
+        }
+    }
+
+}
