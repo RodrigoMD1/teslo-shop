2fd9d407a9b54c8ccba4e403002c05ee20c13f66
rodrigo <rodrigo.martinez224@gmail.com>
2024-05-20 04:27:09 -0300

    v16

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,19 +12,22 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
 
 
+
 @Module({
   imports: [ConfigModule.forRoot(),
 
   TypeOrmModule.forRoot({
     type: 'postgres',
-    host: process.env.DB_HOST, // es el localhost pero esta definido en .env
-    port: +process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    // host: process.env.DB_HOST, // es el localhost pero esta definido en .env
+    // port: +process.env.DB_PORT,
+    //database: process.env.DB_NAME,
+    //username: process.env.DB_USERNAME,
+    // password: process.env.DB_PASSWORD,
+    url: process.env.DB_URL,
+    //ssl: true,
     autoLoadEntities: true,
     synchronize: true,  // cuando este en produccion este no lo voy a querer usar puede que no se necesito o sI, cuando borro alguna columna automaticamente la sincroniza
-
+    logger: 'debug'
   }),
 
   ServeStaticModule.forRoot({
