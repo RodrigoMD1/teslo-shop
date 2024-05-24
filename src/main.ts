291060d64c231e1af5625992aa937be39caf7ebb
rodrigo <rodrigo.martinez224@gmail.com>
2024-05-24 01:18:50 -0300

    v5

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import {  ValidationPipe } from '@nestjs/common';
+import ServerlessHttp = require('serverless-http');
 
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
-  const logger = new Logger('Bootstrap')
+ // const logger = new Logger('Bootstrap');
+  const globalPrefix = '.netlify/functions/main';
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix(globalPrefix);
+  await app.init();
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -18,8 +21,20 @@ async function bootstrap() {
   );
 
 
-  await app.listen(process.env.PORT);
-  logger.log(`app corriendo en el puerto ${process.env.PORT}`);
+  const expressApp = app.getHttpAdapter().getInstance();
+  return ServerlessHttp(expressApp)
+
+  //await app.listen(process.env.PORT);
+  //logger.log(`app corriendo en el puerto ${process.env.PORT}`);
 
 }
-bootstrap();
+
+let server;
+export const handler = async (event, context, callback) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
+
+
+
+
